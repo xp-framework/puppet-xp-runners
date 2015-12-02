9732ca847ecfe30ec0ddea02052f8948ada5d43d
mikey179 <mikey@stubbles.net>
2015-12-02 15:56:46 +0100

    pretend we are Debian
    For some reasons this doesn't work even though lsb-release is available.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 describe 'xp_runners' do
 
+  # Pretend we are Debian, for some reasons this doesn't work even though
+  # lsb-release is available
+  let (:facts) { { :osfamily => 'Debian', :lsbdistid => 'debian' } }
+
   context 'with defaults for all parameters' do
     it { should contain_class('xp_runners') }
   end

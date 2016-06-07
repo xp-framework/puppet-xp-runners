c0f83f10147d8f6fd2ed1a092e060eaffd90f24d
Frank Kleine <frank.kleine@1und1.de>
2016-06-07 11:08:29 +0200

    hmpf, syntax

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,11 +15,11 @@
 #
 class xp_runners::params {
 
-  $manage_package_repo = true,
-  $repo_location = 'https://dl.bintray.com/xp-runners/debian',
-  $repo_release = 'jessie',
-  $repo_repos = 'main',
-  $repo_key_id = '8756C4F765C9AC3CB6B85D62379CE192D401AB61',
-  $repo_key_source = 'https://bintray.com/user/downloadSubjectPublicKey?username=bintray',
-  $repo_requires_https = true,
+  $manage_package_repo = true
+  $repo_location = 'https://dl.bintray.com/xp-runners/debian'
+  $repo_release = 'jessie'
+  $repo_repos = 'main'
+  $repo_key_id = '8756C4F765C9AC3CB6B85D62379CE192D401AB61'
+  $repo_key_source = 'https://bintray.com/user/downloadSubjectPublicKey?username=bintray'
+  $repo_requires_https = true
 }
